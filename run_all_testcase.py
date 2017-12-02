58dadceffcd79c8c8547f4b15e6e2b7015e1bfad
hostpost114 <31546102+hostpost114@users.noreply.github.com>
2017-12-02 23:07:53 +0800

    Update run_all_testcase.py

--- run_all_testcase.py
+++ run_all_testcase.py
@@ -12,7 +12,13 @@ def creat_suit():
         for test_case in test_suit:
             suit.addTest(test_case)
     return suit
+"""1.discover方法里面有三个参数：
 
+-case_dir:这个是待执行用例的目录。
+
+-pattern：这个是匹配脚本名称的规则，test*.py意思是匹配test开头的所有脚本。
+
+-top_level_dir：这个是顶层目录的名称，一般默认等于None就行了。"""
 
 all_test_cases = creat_suit()
 f = open('test.html', 'wb')
